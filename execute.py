a9492153eba2319d768edc35cdbae58b9df364cd
Minechrafti <simi@post.com>
2023-06-16 03:42:16 -0400

    modified execute.py a bit

--- execute.py
+++ execute.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 def getType(string):
+    return '.' + string.split('.')[1]
     for i in range(len(string)):
         if(string[i] == '.'):
             return string[i:]
@@ -9,7 +10,7 @@ def getType(string):
 
 file = str(sys.argv[1])
 ending = getType(file)
-if(ending[0] != '.'):
+if(not ending.__contains__('.')):
     exe = './' + file
     os.system(exe)
 if(ending == '.sh'):
@@ -20,6 +21,13 @@ if(ending == '.py'):
     print("Executing python file...")
     exe = 'python3 ' + file
     os.system(exe)
-
+if(ending == '.c'):
+    print("Executing c file...")
+    exe = f'gcc {file} -o ./tmp; ./tmp; rm ./tmp'
+    os.system(exe)
+if(ending == '.java'):
+    print("Trying to execute java")
+    exe = f'javac {file}'
+    os.system(exe)
 
 
